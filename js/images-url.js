b0e00865732a17a6b9a1be1e836e74f70ac9052b
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2023-04-03 20:14:55 -0300

    Added jquery library

---
+++ js/images-url.js
@@ -0,0 +1,7 @@
+
+
+$('#boton-suma').click(sumarUno);
+
+function sumarUno() {
+    $('#pet').attr('src, https://images.pexels.com/photos/1458916/pexels-photo-1458916.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+}

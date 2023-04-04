c6027cda74c26e06ab94e2cb8b1862e269c9184b
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2023-04-03 22:54:20 -0300

    Added add button function

--- js/images-url.js
+++ js/images-url.js
@@ -1,9 +1,25 @@
-
 //Delete function
-$(document).ready(function() {
-    $('.btn-action').click(function() {
+$(document).ready(function () {
+    $('.btn-action').click(function () {
         let img = $(this).closest('.card').find('.img-card');
         img.attr('src', 'images/no_pet.svg');
     });
 });
+
+
+$(document).ready(function () {
+    let allImg = $('.img-card').toArray();
+    let i = 0;
+    let src = null;
+
+    $('#add-button').click(function () {
+        $(allImg[i]).attr('src', $('#img-url').val());
+        if (i >= allImg.length) {
+            i = 0;
+        }
+    });
+});
+
+
+
 
